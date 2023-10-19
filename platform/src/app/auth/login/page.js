63f99fa7da795ddab086d25c9ad7e49450dbf975
Stream Protocol <contact@streamprotocol.org>
2023-10-19 07:57:04 +0000

    Refactored login page

--- platform/src/app/auth/login/page.js
+++ platform/src/app/auth/login/page.js
@@ -1,37 +1,26 @@
-"use client";
-import Link from "next/link";
-import React from "react";
-import LoginForm from "../../components/Forms/LoginForm";
+import React from 'react';
+import Link from 'next/link';
+import LoginForm from '../../components/Forms/LoginForm';
 
 const Login = () => {
-    return ( <
-        >
-        <
-        div className = " px-3 py-3 bx-shadow sm:px-6 md:px-8 max-w-[500px] mx-auto bg-white rounded-lg" >
-        <
-        div className = "py-6 px-4 md:px-6 lg:px-8 login--bx " >
-        <
-        div className = "mb-8 text-center space-y-2" >
-        <
-        h1 className = "text-2xl capitalize font-medium text-[#111827]" >
-        Welcome back <
-        /h1> <
-        p className = "text-sm " >
-        Don & apos; t have an account ? { " " } <
-        Link href = { "/auth/signup" }
-        className = "text-[#111827]" >
-        Create account <
-        /Link> < /
-        p > <
-        /div>
-
-        <
-        LoginForm / >
-        <
-        /div> < /
-        div > <
-        />
-    );
+  return (
+    <div className="max-w-[500px] mx-auto bg-white rounded-lg px-3 py-3 bx-shadow sm:px-6 md:px-8">
+      <div className="px-4 py-6 md:px-6 lg:px-8 login--bx">
+        <div className="mb-8 text-center space-y-2">
+          <h1 className="text-2xl font-medium capitalize text-[#111827]">
+            Welcome back
+          </h1>
+          <p className="text-sm">
+            Don't have an account?{' '}
+            <Link href="/auth/signup">
+              <a className="text-[#111827]">Create account</a>
+            </Link>
+          </p>
+        </div>
+        <LoginForm />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
